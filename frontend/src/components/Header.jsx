e8f8cc19b3465b4d91499aeb52c95241a15f5dba
Harsha-Hemanth <158434803+Harsha-Hemanth@users.noreply.github.com>
2024-12-21 15:52:12 +0530

    added all required images in to assets and designed logo

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from './Logo'
+
+
+
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md'>
+        <div className='h-full container mx-auto flex items-center justify-between'>
+            <div className='h-full'>
+                <Logo w={90} h={50}/>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
